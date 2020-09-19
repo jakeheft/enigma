e048c90f29b06022c893bfda6f41a888b6e77df0
Jake Heft <jakeheft@gmail.com>
2020-09-19 15:21:47 -0600

    Add test for shift value

--- test/a_shift_test.rb
+++ test/a_shift_test.rb
@@ -8,4 +8,14 @@ class AShiftTest < Minitest::Test
 
     assert_instance_of AShift, a_shift
   end
+
+  def test_it_can_get_shift_value
+    main_shift = Shift.new
+    a_shift = AShift.new
+
+    main_shift.stubs(:generate_key).returns("12345")
+    main_shift.stubs(:date).returns("091920")
+
+    assert_equal 18, a_shift.shift_value
+  end
 end
