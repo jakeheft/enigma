0471ed1b224dda4c388ae4dcb546ece437604da6
Jake Heft <jakeheft@gmail.com>
2020-09-19 15:50:30 -0600

    Add test for getting date shift

--- test/a_shift_test.rb
+++ test/a_shift_test.rb
@@ -5,21 +5,28 @@ require './lib/a_shift'
 class AShiftTest < Minitest::Test
   def test_it_exists
     main_shift = Shift.new("12345", "091920")
-    a_shift = AShift.new("12345", "091920")
+    a_shift = AShift.new("12345", "6400")
 
     assert_instance_of AShift, a_shift
   end
 
   def test_it_can_get_its_key_shift
     main_shift = Shift.new("12345", "091920")
-    a_shift = AShift.new("12345", "091920")
+    a_shift = AShift.new("12345", "6400")
 
     assert_equal "12", a_shift.key_shift
   end
 
+  def test_it_can_get_its_date_shift
+    main_shift = Shift.new("12345", "091920")
+    a_shift = AShift.new("12345", "6400")
+
+    assert_equal "6", a_shift.date_shift
+  end
+
   def test_it_can_get_shift_value
     main_shift = Shift.new("12345", "091920")
-    a_shift = AShift.new("12345", "091920")
+    a_shift = AShift.new("12345", "6400")
 
     main_shift.stubs(:generate_key).returns("12345")
     main_shift.stubs(:date).returns("091920")
