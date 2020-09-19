e79be2980d28c96a9b85f4df6a47038f255aab20
Jake Heft <jakeheft@gmail.com>
2020-09-19 11:48:47 -0600

    Add test to check for alphabet

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,6 +3,14 @@ require './lib/shift'
 
 class ShiftTest < Minitest::Test
   def test_it_exists
-    shift = Shift.new
+    main_shift = Shift.new
+  end
+
+  def test_it_has_an_alphabet_that_can_be_called_on
+    main_shift = Shift.new
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, main_shift.alpha
+    assert_equal "j", main_shift.alpha[9]
   end
 end
