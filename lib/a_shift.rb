9f3e035e78e744f26efebe82d8ffa0b779a4a49e
Jake Heft <jakeheft@gmail.com>
2020-09-19 16:04:13 -0600

    Add shift alphabet method

--- lib/a_shift.rb
+++ lib/a_shift.rb
@@ -1,7 +1,10 @@
 class AShift < Shift
+  attr_reader :shifted_alpha
+
   def initialize(key, date_shift_all)
     @key = key
     @date_shift_all = date_shift_all
+    @shifted_alpha = shift_alphabet
   end
   # does key_shift need to go in shift class and have each sub class pull from that method and input what parts it needs?
   def key_shift
@@ -14,5 +17,9 @@ class AShift < Shift
 
   def shift_value
     key_shift.to_i + date_shift.to_i
+  end
+
+  def shift_alphabet
+    (("a".."z").to_a << " ").rotate(shift_value)
   end
 end
