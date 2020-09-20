298bd388547bd7be3c8ca9d733afb74ddce06ff1
Jake Heft <jakeheft@gmail.com>
2020-09-20 14:38:29 -0600

    Add test for incoming characters A

--- test/a_shift_test.rb
+++ test/a_shift_test.rb
@@ -7,18 +7,22 @@ class AShiftTest < Minitest::Test
   end
 
   def test_it_exists
+    skip
     assert_instance_of AShift, @a_shift
   end
 
   def test_it_can_get_its_key_shift
+    skip
     assert_equal "12", @a_shift.key_shift
   end
 
   def test_it_can_get_its_date_shift
+    skip
     assert_equal "6", @a_shift.date_shift
   end
 
   def test_it_can_get_shift_value
+    skip
     @main_shift.stubs(:generate_key).returns("12345")
     @main_shift.stubs(:date).returns("091920")
 
@@ -26,9 +30,14 @@ class AShiftTest < Minitest::Test
   end
 
   def test_it_can_get_shifted_alphabet
+    skip
     expected = ["s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"]
 
     assert_equal expected, @a_shift.shift_alphabet
     assert_equal expected, @a_shift.shifted_alpha
+  end
+
+  def test_it_can_get_incoming_characters
+    assert_equal ["h", "o", "r"], @a_shift.incoming_chars
   end
 end
