8f7aa2c334ae74ef4eee31ce20226d35fc3bd826
Jake Heft <jakeheft@gmail.com>
2020-09-20 14:25:22 -0600

    Add test for message

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -2,12 +2,14 @@ require './test/test_helper'
 
 class ShiftTest < Minitest::Test
   def test_it_exists
+    skip
     main_shift = Shift.new
 
     assert_instance_of Shift, main_shift
   end
 
   def test_it_instantiates_each_shift_letter_object
+    skip
     main_shift = Shift.new
 
     assert_instance_of AShift, main_shift.a_shift
@@ -17,6 +19,7 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_has_an_alphabet_that_can_be_called_on
+    skip
     main_shift = Shift.new
 
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
@@ -26,7 +29,14 @@ class ShiftTest < Minitest::Test
     assert_nil main_shift.alpha[28]
   end
 
+  def test_it_has_a_message
+    main_shift = Shift.new("hello world")
+
+    assert_equal "hello world", main_shift.message
+  end
+
   def test_it_can_generate_a_5_digit_key_that_will_not_change
+    skip
     main_shift = Shift.new
     expected = main_shift.key
 
@@ -38,6 +48,7 @@ class ShiftTest < Minitest::Test
   end
 ### PUT IN A TEST FOR PULLING THE DATE WHEN YOU'VE FIGURED IT OUT
   def test_it_can_get_the_date_for_the_shift
+    skip
     main_shift = Shift.new
 
     assert_equal 6, main_shift.date.length
@@ -46,4 +57,15 @@ class ShiftTest < Minitest::Test
     assert_equal "6400", main_shift.date_shift_all
   end
   ### When all letter shifts built, create collection with each so they can be run through enumerable
+
+  # def test_it_can_route_a_string_to_proper_shift_subclasses
+  #   main_shift = Shift.new("hello world")
+  #
+  #   main_shift.disburse_message
+  #
+  #   assert_equal ["h", "o", "r"], main_shift.a_shift.incoming_chars
+  #   assert_equal ["e", " ", "l"], main_shift.b_shift.incoming_chars
+  #   assert_equal ["l", "w", "d"], main_shift.c_shift.incoming_chars
+  #   assert_equal ["l", "o"], main_shift.d_shift.incoming_chars
+  # end
 end
