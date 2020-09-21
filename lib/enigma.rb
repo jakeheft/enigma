28fb603cbb3e1406c5d42b15b919027622ccb942
Jake Heft <jakeheft@gmail.com>
2020-09-21 09:45:11 -0600

    Add attr reader methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,10 @@
 class Enigma
+  attr_reader :message, :key, :date
+  
   def initialize(message, key, date)
     @message = message
+    @key = key
+    @date = date
+    @main_shift = Shift.new(@message, @key, @date)
   end
 end
