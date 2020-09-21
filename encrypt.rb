7630a4f3d4aba34449e3fb4f35357936a2a4e37b
Jake Heft <jakeheft@gmail.com>
2020-09-21 13:42:35 -0600

    Update gitignore

--- encrypt.rb
+++ encrypt.rb
@@ -4,12 +4,13 @@ handle = File.open(ARGV[0], "r")
 
 message = handle.read
 
-enigma = Enigma.new(message)
-require "pry"; binding.pry
+enigma = Enigma.new(message, "12345", "091920")
+enigma.encrypt(message, "12345", "091920")
 handle.close
+### trying to figure out why encrypted file is blank (wasn't running enigma.encrypt) and now having argument issues
 
 writer = File.open(ARGV[1], "w")
-
-writer.write("PUT CIPHERTEXT HERE")
+# require "pry"; binding.pry
+writer.write(enigma.encryption)
 writer.close
 puts "Created 'encrypted.txt' with the key #{enigma.key} and date #{enigma.date}"
