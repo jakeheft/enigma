d3659d915a57d24aa494ac24aa7f2b7e8766f4ac
Jake Heft <jakeheft@gmail.com>
2020-09-20 18:11:34 -0600

    Change format to output to outgoing chars

--- lib/shiftable.rb
+++ lib/shiftable.rb
@@ -8,6 +8,6 @@ module Shiftable
   end
 
   def run_shift
-    @incoming_chars.map { |char| @shifted_alpha[char[1]] }
+    @outgoing_chars = @incoming_chars.map { |char| @shifted_alpha[char[1]] }
   end
 end
