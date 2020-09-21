5ad582d8936160582f1bbe6e71a7fe26fa4b6c5b
Jake Heft <jakeheft@gmail.com>
2020-09-21 11:26:05 -0600

    Add message into argument for tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,7 +2,7 @@ require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists
-    enigma = Enigma.new
+    enigma = Enigma.new("hello world")
 
     assert_instance_of Enigma, enigma
   end
@@ -16,7 +16,7 @@ class EnigmaTest < Minitest::Test
   # end
 
   def test_it_can_generate_a_5_digit_key_that_will_not_change
-    enigma = Enigma.new
+    enigma = Enigma.new("hello world")
     expected = enigma.key
 
     assert_equal 5, enigma.key.length
