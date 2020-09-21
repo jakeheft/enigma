a2a1ac4d2acdb4f11ac846351704901e444f3514
Jake Heft <jakeheft@gmail.com>
2020-09-21 15:47:49 -0600

    Update encrypt file, working

--- encrypt.rb
+++ encrypt.rb
@@ -2,15 +2,16 @@ require './test/test_helper'
 
 handle = File.open(ARGV[0], "r")
 
-message = handle.read
+message = (handle.read).downcase
 
-enigma = Enigma.new(message, "12345", "091920")
-enigma.encrypt(message, "12345", "091920")
+enigma = Enigma.new(message, "091920")
+
+enigma.encrypt(message, "091920")
 handle.close
-### trying to figure out why encrypted file is blank (wasn't running enigma.encrypt) and now having argument issues
 
 writer = File.open(ARGV[1], "w")
-# require "pry"; binding.pry
+
 writer.write(enigma.encryption)
 writer.close
+
 puts "Created 'encrypted.txt' with the key #{enigma.key} and date #{enigma.date}"
